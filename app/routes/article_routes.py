87de7237ab22c640c37645cc9729883f030120f9
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2025-07-18 14:37:27 +0200

    ajout du get article by id et get article by category

--- app/routes/article_routes.py
+++ app/routes/article_routes.py
@@ -6,6 +6,7 @@ from bson import ObjectId
 
 router = APIRouter()
 
+# fonction pour convetir data de mongodb en un objet pour le frontend 
 def article_serializer(data) -> dict:
     return {
         "id": str(data["_id"]),  
@@ -19,16 +20,37 @@ def article_serializer(data) -> dict:
         "auteur": data["auteur"],
     }
 
+#Route pour GET articles
 @router.get("/articles")
 async def get_articles():
     articles = articles_collection.find()
     return [article_serializer(article) for article in articles]
 
 
+#Route pour GET articles BY id
+@router.get("/articles/{id}")
+async def get_article_by_id(id: str):
+    try:
+        obj_id = ObjectId(id)
+    except:
+        raise HTTPException(status_code=400, detail="ID invalide")
+
+    article = articles_collection.find_one({"_id": obj_id})
+    if article:
+        return article_serializer(article)
+    raise HTTPException(status_code=404, detail="Article non trouvé")
 
 
+#Route pour GET articles BY category
+@router.get("/articles/category/{categorie}")
+async def get_articles_by_categorie(categorie: str):
+    articles = articles_collection.find({"categorie": categorie})
+    return [article_serializer(article) for article in articles]
+
+
+
+#Royute pour POST article
 @router.post("/article")
-
 async def create_article(article_data: article):
     article_dict = jsonable_encoder(article_data)
     # Supprime 'id' ou '_id' si présents pour éviter doublons
